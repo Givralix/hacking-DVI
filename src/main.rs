ef33471674d4172a80b5718fbedab40c7e960d84
cqql <cqql.0@proton.me>
2025-02-21 20:36:15 +0100

    encoding and decoding implemented correctly

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 fn main() {
     let args: Vec<_> = std::env::args().collect();
-    let img = image::open(args[1].clone()).unwrap();
+
+    let filename = &String::from("pack.jpg");
+    let path = args.get(1).unwrap_or(filename);
+
+    let img = image::open(path.clone()).unwrap();
+    
     let mut buffer = img.into_rgb8();
     let mut cnt = [0; 3];
     for pixel in buffer.pixels_mut() {
-        let channel_0 = encode(pixel.0[0], cnt[0], true, false, false);
-        cnt[0] = channel_0.1;
-        let channel_1 = encode(pixel.0[1], cnt[1], true, false, false);
-        cnt[1] = channel_1.1;
-        let channel_2 = encode(pixel.0[2], cnt[2], true, false, false);
-        cnt[2] = channel_2.1;
-        *pixel = image::Rgb([channel_0.0 as u8, channel_1.0 as u8, channel_2.0 as u8]);
+        // encode
+        let (channel_0, count_0) = encode(pixel.0[0], cnt[0], true, false, false);
+        cnt[0] = count_0;
+        
+        let (channel_1, count_1) = encode(pixel.0[1], cnt[1], true, false, false);
+        cnt[1] = count_1;
+        
+        let (channel_2, count_2) = encode(pixel.0[2], cnt[2], true, false, false);
+        cnt[2] = count_2;
+
+        // decode
+        let (channel_0, _, _) = decode(channel_0, true);
+        let (channel_1, _, _) = decode(channel_1, true);
+        let (channel_2, _, _) = decode(channel_2, true);
+        
+        *pixel = image::Rgb([channel_0 as u8, channel_1 as u8, channel_2 as u8]);
     }
 
-    buffer.save(format!("encoded-{}", args[1])).unwrap();
+    buffer.save(format!("encoded-{}", path)).unwrap();
 }
 
 fn encode(color: u8, cnt_prev: isize, data_enable: bool, c0: bool, c1: bool) -> (u16, isize) {
@@ -30,16 +44,16 @@ fn encode(color: u8, cnt_prev: isize, data_enable: bool, c0: bool, c1: bool) -> 
     let mut q_out: u16 = 0;
     let mut cnt = cnt_prev;
     q_m = color as u16 & 0b0000_0001;
-    
+    // 0111_0010
     if color.count_ones() > 4 || (color.count_ones() == 4 && color & 0b0000_0001 == 0) {
         for i in 0..=6 {
             let mask = 1 << i;
-            q_m = !((q_m & mask) ^ (color as u16 & (mask << 1)));
+            q_m += (!(((q_m & mask) << 1) ^ (color as u16 & (mask << 1)))) & (mask << 1);
         }
     } else {
         for i in 0..=6 {
             let mask = 1 << i;
-            q_m = (q_m & mask) ^ (color as u16 & (mask << 1));
+            q_m += (((q_m & mask) << 1) ^ (color as u16 & (mask << 1))) & (mask << 1);
         }
         q_m = q_m.saturating_add(0b0000_0001_0000_0000);
     }
@@ -77,6 +91,37 @@ fn encode(color: u8, cnt_prev: isize, data_enable: bool, c0: bool, c1: bool) -> 
     (q_out, cnt)
 }
 
-fn decode() {
-    todo!()
+fn decode(q_out: u16, data_enable: bool) -> (u8, bool, bool) {
+    if !data_enable {
+        return match q_out {
+            0b0010101011 => (0, false, false),
+            0b1101010100 => (0, false, true),
+            0b0010101010 => (0, true, false),
+            0b1101010101 => (0, true, true),
+            _            => (0b0101_0101, false, false) // this shouldn't happen (but it will because we want it to break)
+        }
+    }
+
+    let d9 = q_out & 0b10_0000_0000 != 0;
+    let d8 = q_out & 0b01_0000_0000 != 0;
+    let mut color = q_out as u8;
+
+    if d9 {
+        color = !color;
+    }
+
+    let mut new_color = color & 0b0000_0001;
+    if d8 {
+        for i in 0..=6 {
+            let mask = 1 << i;
+            new_color += ((((color & mask) << 1) ^ (color & (mask << 1)))) & (mask << 1);
+        }
+    } else {
+        for i in 0..=6 {
+            let mask = 1 << i;
+            new_color += (!((color & mask) << 1) ^ (color & (mask << 1))) & (mask << 1);
+        }
+    }
+
+    (new_color, false, false)
 }
